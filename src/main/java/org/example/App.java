3e4f3ab84b902f62539e182b7d1a5767f7528d2a
AkashSingh-JU23GBG <Akash.singh@iths.se>
2023-09-16 19:04:54 +0200

    del 2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner scanner=new Scanner(System.in);
 
             String menu= """
@@ -22,20 +23,62 @@ public class App {
             if (choice.equals("1"));
             int[] elpriser=new int [24];
             for (int i=0; i<24; i++){
-                System.out.print("Skriv in elpriset för en timme)"+i+":");
+                System.out.print("Skriv in elpriset för timme "+i+":");
                 elpriser[i]=scanner.nextInt();
 
             }
             System.out.println("Elpriser för dygnet:");
             for (int i=0; i<24; i++){
-                System.out.println("Timme" + i+":"+elpriser[i]+"öre per kwh")
+                System.out.println("Timme" + i+":"+elpriser[i]+"öre per kwh");
+
+                if (choice.equals("2"));
+                int lägstaPris= elpriser[0];
+                int lägstaTimme= 0;
+                for (i = 1; i < 24; i++){
+                    if(elpriser[i]<lägstaPris){
+                        lägstaPris=elpriser[i];
+                        lägstaTimme=i;
+                    }
+                }
+
+                int högstaPris=elpriser[0];
+                int högstaTimme=0;
+                for (i = 1; i<24; i++){
+                    högstaPris=elpriser [i];
+                    högstaTimme=i;
+                }
             }
+        int totalPris=0;
+        for (int i=0; i<24; i++){
+            totalPris+= elpriser[i];
+                }
+        int medelPris = (int) totalPris/24;
+        System.out.println("Elpriser för dygnet:");
+        for (int i=0; i<24;i++){
+            System.out.println("Timme"+i +":"+ elpriser[i] +"öre perKWh");
+        }
+        if (choice.equals(2));
+        int minpris =elpriser[0];
+        int maxPris= elpriser[0];
+        int totalpris= elpriser[0];
 
+        for (int i =0; i<24; i++){
+            if(elpriser[i] < minpris){
+                minpris=elpriser[i];
+            }
+            if (elpriser[i]>maxPris){
+                maxPris+=elpriser[i];
+            }
+            totalpris+=elpriser[i];
+        }
 
+        int medelpris=(int) totalPris/24;
 
-
-
+        System.out.println("Lägsta priset: " + minpris+ "öre per kWh");
+        System.out.println("Högsta priset:"+maxPris+"öre per kHw");
+        System.out.println("Dygnets medelpris:"+medelPris+"öre per kHw");
 
 
         }
     }
+}
