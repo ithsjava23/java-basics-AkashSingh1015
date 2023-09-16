3c042a4e1a932f87c342c1bc120ce2c3d234e88c
AkashSingh-JU23GBG <Akash.singh@iths.se>
2023-09-16 16:56:28 +0200

    del 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,41 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello There!");
+        Scanner scanner=new Scanner(System.in);
+
+            String menu= """
+                    Elpriser
+                    ========
+                    1. Inmatning
+                    2. Min, Max och Medel
+                    3. Sortera
+                    4. Bästa Laddningstid (4h)
+                    e. Avsluta
+                    """;
+            System.out.print(menu);{
+            String choice= scanner.nextLine();
+
+            if (choice.equals("1"));
+            int[] elpriser=new int [24];
+            for (int i=0; i<24; i++){
+                System.out.print("Skriv in elpriset för en timme)"+i+":");
+                elpriser[i]=scanner.nextInt();
+
+            }
+            System.out.println("Elpriser för dygnet:");
+            for (int i=0; i<24; i++){
+                System.out.println("Timme" + i+":"+elpriser[i]+"öre per kwh")
+            }
+
+
+
+
+
+
+
+        }
     }
-}
