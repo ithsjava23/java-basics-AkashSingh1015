88489863a168cc7bb3010b20a7d15ea957eef1e4
AkashSingh-JU23GBG <Akash.singh@iths.se>
2023-09-17 19:34:39 +0200

    ..

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,82 +3,78 @@ package org.example;
 import java.util.Scanner;
 
 public class App {
+private static void exitWithLowerCaseE() {
+    System.out.println("programmet avslutas");
+    System.exit(0);
+}
+    public static void main(String[] args){
 
-    public static void main(String[] args)
+
     {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        int[] elpriser = new int[24];
 
-            String menu= """
-                    Elpriser
-                    ========
-                    1. Inmatning
-                    2. Min, Max och Medel
-                    3. Sortera
-                    4. Bästa Laddningstid (4h)
-                    e. Avsluta
-                    """;
-            System.out.print(menu);{
-            String choice= scanner.nextLine();
+        String menu = """
+                Elpriser
+                ========
+                1.Inmatning
+                2.Min, Max och Medel
+                3.Sortera
+                4.Bästa Laddningstid (4h)
+                e. Avsluta
+                """;
+        System.out.println(menu);
+        menu = scanner.nextLine();
 
-            if (choice.equals("1"));
-            int[] elpriser=new int [24];
-            for (int i=0; i<24; i++){
-                System.out.print("Skriv in elpriset för timme "+i+":");
-                elpriser[i]=scanner.nextInt();
+        switch (menu) {
+            case "1":
+                elpriser = new int[24];
+                for (int i = 0; i < 24; i++){
+                    System.out.print("Skriv in elpriset för timme " + i + ": ");
+                    elpriser[i]=scanner.nextInt();
 
-            }
-            System.out.println("Elpriser för dygnet:");
-            for (int i=0; i<24; i++){
-                System.out.println("Timme" + i+":"+elpriser[i]+"öre per kwh");
+                }
+                System.out.println("Elpriser för dygnet:");
+                for (int i = 0; i < 24; i++){
+                    System.out.println("Timme " + i + ": " + elpriser[i] + " öre per kWh");
+                }
+                break;
+            case "2":
+                // Min, Max och Medel-alternativ
+                int minPris = elpriser[0];
+                int maxPris = elpriser[0];
+                int totalPris = elpriser[0];
+                int minTimme = 1;
+                int maxTimme = 1;
 
-                if (choice.equals("2"));
-                int lägstaPris= elpriser[0];
-                int lägstaTimme= 0;
-                for (i = 1; i < 24; i++){
-                    if(elpriser[i]<lägstaPris){
-                        lägstaPris=elpriser[i];
-                        lägstaTimme=i;
+                for (int i = 1; i < 24; i++) {
+                    if (elpriser[i] < minPris) {
+                        minPris = elpriser[i];
+                        minTimme = i + 1;
                     }
+                    if (elpriser[i] > maxPris) {
+                        maxPris = elpriser[i];
+                        maxTimme = i + 1;
+                    }
+                    totalPris += elpriser[i];
                 }
 
-                int högstaPris=elpriser[0];
-                int högstaTimme=0;
-                for (i = 1; i<24; i++){
-                    högstaPris=elpriser [i];
-                    högstaTimme=i;
-                }
-            }
-        int totalPris=0;
-        for (int i=0; i<24; i++){
-            totalPris+= elpriser[i];
-                }
-        int medelPris = (int) totalPris/24;
-        System.out.println("Elpriser för dygnet:");
-        for (int i=0; i<24;i++){
-            System.out.println("Timme"+i +":"+ elpriser[i] +"öre perKWh");
+                double medelPris = (double) totalPris / 24;
+
+                System.out.println("Lägsta pris: " + minPris + " öre (Timme " + minTimme + ")");
+                System.out.println("Högsta pris: " + maxPris + " öre (Timme " + maxTimme + ")");
+                System.out.println("Dygnsmedelpris: " + medelPris + " öre");
+                break;
+            case "e":
+            case "E":
+                exitWithLowerCaseE();
+                System.out.println("Programmet avslutas.");
+                scanner.close();
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Ogiltigt val. Försök igen.");
         }
-        if (choice.equals(2));
-        int minpris =elpriser[0];
-        int maxPris= elpriser[0];
-        int totalpris= elpriser[0];
-
-        for (int i =0; i<24; i++){
-            if(elpriser[i] < minpris){
-                minpris=elpriser[i];
-            }
-            if (elpriser[i]>maxPris){
-                maxPris+=elpriser[i];
-            }
-            totalpris+=elpriser[i];
-        }
-
-        int medelpris=(int) totalPris/24;
-
-        System.out.println("Lägsta priset: " + minpris+ "öre per kWh");
-        System.out.println("Högsta priset:"+maxPris+"öre per kHw");
-        System.out.println("Dygnets medelpris:"+medelPris+"öre per kHw");
-
-
         }
     }
 }
